e53937b90c1639e0ee2fc69b4e49e6cdb7bc28eb
KaichCosta <kaichhc@gmail.com>
2025-09-13 23:04:00 -0300

    Finalizei o Projeto, agora vou adicionar opção de Converter de Euro e Dolar pra Real

--- convertidor-de-moedas/script.js
+++ convertidor-de-moedas/script.js
@@ -1,14 +1,60 @@
 const acaoBotao = document.querySelector("#Converter")
+const selectMoeda = document.querySelector("#moeda-para")
 
+const dolarteste = 5.8
+const euroteste = 6.5
+
+selectMoeda.addEventListener("change", function(){
+
+    if (selectMoeda.value =="dolar") {
+        const imagem = document.querySelector('img[alt="moeda-convertida"]')
+        imagem.src = "./assets/estados-unidos.png"
+
+        const descricao_da_moeda = document.querySelector(".nome-moeda-convertida")
+        descricao_da_moeda.innerHTML = "Dólar"
+        
+    }
+
+    if (selectMoeda.value =="euro") {
+        const imagem = document.querySelector('img[alt="moeda-convertida"]')
+        imagem.src = "./assets/euro.png"
+
+        const descricao_da_moeda = document.querySelector(".nome-moeda-convertida")
+        descricao_da_moeda.innerHTML = "Euro"
+
+    }
+
+    converterMoeda()
+})  
 function converterMoeda(event){
-    event.preventDefault()
+    if (event) event.preventDefault()
+
     const valorConverter = document.querySelector("#valor").value
-    console.log("Valor que está no input é " + valorConverter)
+    const valor_a_converter = document.getElementById("valor-inicial")
+    const valor_convertido = document.getElementById("valor-final") 
 
-    const dolarteste = 6
+    if (selectMoeda.value == "dolar") {
+        valor_convertido.innerHTML = new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'USD'
+        }).format(valorConverter / dolarteste)
+    }
 
-    const conversao = (valorConverter / dolarteste).toFixed(2)
-    console.log("Valor da conversão é " + conversao)
+    if (selectMoeda.value =="euro") {
+        valor_convertido.innerHTML = new Intl.NumberFormat('de-DE', {
+            style: 'currency',
+            currency: 'EUR'
+        }).format((valorConverter / euroteste))
+
+        const imagem = document.querySelector('img[alt="moeda-convertida"]')
+        imagem.src = "./assets/euro.png"
+    }
+
+    
+    valor_a_converter.innerHTML = new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL' }).format(valorConverter)
+
 }
 
 acaoBotao.addEventListener("click", converterMoeda)
